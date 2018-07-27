e00c7546ccf3f6843fca69121cd678485177e61e
deborah-chasman <deborah.chasman@gmail.com>
2018-07-27 11:50:17 -0500

    Added example data

---
+++ run_example_command.sh
@@ -0,0 +1,2 @@
+mkdir output
+./escarole example_input/time_order.txt example_input/ortho_map.txt 5 example_input/time_tree.txt example_input/ashton_config.txt none output learn ashton_2hr uniform 0.8 0.2 1> run_example.log 2> run_example.err
